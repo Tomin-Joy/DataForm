2108b5147528a73f339ed4e1c576a7edda83dd9e
Tomin Joy <67215154+Tomin-Joy@users.noreply.github.com>
2022-02-18 13:42:31 +0530

    Add files via upload

--- src/DataForm.java
+++ src/DataForm.java
@@ -1,16 +1,15 @@
 import com.mysql.cj.jdbc.exceptions.MysqlDataTruncation;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.*;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
+import java.sql.*;
 
 public class DataForm {
     public static void main(String[] args) throws  Exception{
         Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection con = DriverManager.getConnection("jdbc:mysql://localhost/", "root", "");
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost/","cipherguys","ATM");
         Statement smt = con.createStatement();
         try{
             smt.execute("use Data");
@@ -26,23 +25,19 @@ public class DataForm {
         App app = new App();
         app.run(con,smt);
 
-
-
     }
 
 }
 
 class App extends JFrame implements ActionListener, FocusListener {
 
-    TextField tName;
-    TextField tAge;
-    TextField tEmail;
-    TextField tPhone;
-    JButton submit;
-    Font f1;
-    Font f2;
+    TextField tName,tAge,tEmail,tPhone;
+    JButton submit,showAll,del;
+    Font f1,f2;
     Statement smt;
     Connection con;
+    JFrame delBox = new JFrame();
+
 
     void run(Connection con,Statement smt) {
         this.smt=smt;
@@ -70,6 +65,8 @@ class App extends JFrame implements ActionListener, FocusListener {
         Label phone = new Label("Phone  : ", 2);
 
         submit = new JButton("Submit");
+        showAll = new JButton("Show all");
+        del = new JButton("Delete");
 
         tName = new TextField("Enter name here..", 20);
         tAge = new TextField("Enter age here..", 20);
@@ -83,8 +80,17 @@ class App extends JFrame implements ActionListener, FocusListener {
 
         submit.setFont(fLabel);
         submit.setBackground(Color.GREEN);
+        submit.setForeground(Color.WHITE);
+        showAll.setFont(fLabel);
+        showAll.setBackground(Color.BLUE);
+        showAll.setForeground(Color.WHITE);
+        del.setFont(fLabel);
+        del.setBackground(Color.RED);
+        del.setForeground(Color.WHITE);
 
         submit.addActionListener(this);
+        showAll.addActionListener(this);
+        del.addActionListener(this);
         tName.addFocusListener(this);
         tAge.addFocusListener(this);
         tEmail.addFocusListener(this);
@@ -110,6 +116,8 @@ class App extends JFrame implements ActionListener, FocusListener {
         pPhone.add(phone);
         pPhone.add(tPhone);
         pSubmit.add(submit);
+        pSubmit.add(del);
+        pSubmit.add(showAll);
 
         add(pTitle);
         add(pName);
@@ -123,32 +131,72 @@ class App extends JFrame implements ActionListener, FocusListener {
         setLayout(new GridLayout(6, 1));
         setVisible(true);
         setTitle("Data Form V-1.0.2");
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
 
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        System.out.print("\nDetails :  \n\n");
-        System.out.println("Name         : " + tName.getText());
-        System.out.println("Age          : " + tAge.getText());
-        System.out.println("Email        : " + tEmail.getText());
-        System.out.println("Phone number : " + tPhone.getText());
-        System.out.print("\n");
-        try {
-            String query = String.format("insert into Data values(\"%s\",\"%s\",\"%s\",\"%s\")",tName.getText(),tAge.getText(),tEmail.getText(),tPhone.getText());
-            smt.execute(query);
-        } catch (MysqlDataTruncation ex){
-            JOptionPane.showMessageDialog(this,"Data too long");
-        }
-        catch (Exception ex) {
-            ex.printStackTrace();
-        }
+        JButton b = (JButton) e.getSource();
+        if(b==submit){
+            System.out.print("\nDetails :  \n\n");
+            System.out.println("Name         : " + tName.getText());
+            System.out.println("Age          : " + tAge.getText());
+            System.out.println("Email        : " + tEmail.getText());
+            System.out.println("Phone number : " + tPhone.getText());
+            System.out.print("\n");
+            try {
+                String query = String.format("insert into Data values(\"%s\",\"%s\",\"%s\",\"%s\")",tName.getText(),tAge.getText(),tEmail.getText(),tPhone.getText());
+                smt.execute(query);
+            } catch (MysqlDataTruncation ex){
+                JOptionPane.showMessageDialog(this,"Data too long");
+            }
+            catch (Exception ex) {
+                ex.printStackTrace();
+            }
 
-        tAge.setText("Enter age here..");
-        tEmail.setText("Enter email here..");
-        tName.setText("Enter name here..");
-        tPhone.setText("Enter phone number here..");
+            tAge.setText("Enter age here..");
+            tEmail.setText("Enter email here..");
+            tName.setText("Enter name here..");
+            tPhone.setText("Enter phone number here..");
+        }else if(b==showAll){
+            try {
+                ResultSet d = smt.executeQuery("select * from data;");
+                String[] columnNames = {"Name", "Age", "Email", "Phone"};
+                JFrame detailsBox = new JFrame();
+                DefaultTableModel model = new DefaultTableModel();
+                model.setColumnIdentifiers(columnNames);
+                JTable details = new JTable();
+                details.setModel(model);
+                detailsBox.setLayout(new FlowLayout());
+                detailsBox.setSize(500,400);
+                details.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
+                details.setFillsViewportHeight(true);
+                JScrollPane scroll = new JScrollPane(details);
+                scroll.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+                scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+
+                detailsBox.setVisible(true);
+                detailsBox.add(scroll);
+
+                while (d.next()){
+                    String str = d.getString("Name")+" "+d.getString("Age");
+                    String roll = d.getString("Name");
+                    String name = d.getString("Age");
+                    String cl = d.getString("Email");
+                    String sec = d.getString("Phone");
+                    model.addRow(new Object[]{roll, name, cl, sec});
+
+                }
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+
+            }
+        }else if(b==del){
+            delBox.setSize(400,400);
+            delBox.setVisible(true);
+            delBox.setLayout(new BorderLayout());
+            JLabel tittle = new JLabel();
+        }
     }
 
 
